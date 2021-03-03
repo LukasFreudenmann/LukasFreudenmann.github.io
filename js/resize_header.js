ad9e49de0971334c1edbe93cb09c1e62eb5c7b91
Lukas <lukas.freudenmann@web.de>
2021-03-03 17:34:20 +0100

    Added image gallery

--- js/resize_header.js
+++ js/resize_header.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.height = "33%";
         document.getElementById("header-text").style.fontSize = "24px";
+        document.getElementById("header-text2").style.fontSize = "14px";
     } else {
-        document.getElementById("header").style.height = "50%";
         document.getElementById("header-text").style.fontSize = "48px";
+        document.getElementById("header-text2").style.fontSize = "20px";
     }
 }
