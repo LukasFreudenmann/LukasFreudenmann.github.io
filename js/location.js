c6d0adebb8dea2ba3bd9929c30e60335019acf2c
Lukas <lukas.freudenmann@web.de>
2021-03-19 16:05:48 +0100

    Sorting elements by distance

--- js/location.js
+++ js/location.js
@@ -1,6 +1,6 @@
-var distances = ["MarktplatzDist", "BotatischerGartenDist", "RheinhafenDist", "LernzentrumDist", "ZKMDist",
+const distances = ["MarktplatzDist", "BotatischerGartenDist", "RheinhafenDist", "LernzentrumDist", "ZKMDist",
     "RheinbrückeDist", "SchlossGottesaueDist", "SchlossDist", "TurmbergDist"];
-var positions = [
+const positions = [
     49.00927401131935, 8.403893297438733,
     49.013004654388766, 8.39966357828218,
     49.01523252064477, 8.344084445965663,
@@ -11,6 +11,8 @@ var positions = [
     49.0131014133627, 8.4043295523524,
     48.996914024725626, 8.48536739549212
 ];
+
+let elements = ["elem1", "elem2", "elem3", "elem4", "elem5", "elem6", "elem7", "elem8", "elem9"]
 
 
 function init() {
@@ -49,35 +51,56 @@ function showPosError(error) {
     }
 }
 
-function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 
 //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
 function calcDistances(position) {
-    var aktLat = position.coords.latitude;
-    var aktLon = position.coords.longitude;
+    let aktLat = position.coords.latitude;
+    let aktLon = position.coords.longitude;
+    let distanceList = [];
 
-    for (i = 0; i < distances.length; i++) {
-        var R = 6371; // km
-        var dLat = toRad(positions[2 * i] - aktLat);
-        var dLon = toRad(positions[2 * i + 1] - aktLon);
-        var lat1 = toRad(aktLat);
-        var lat2 = toRad(positions[2 * i]);
+    for (let i = 0; i < distances.length; i++) {
+        const R = 6371; // km
+        let dLat = toRad(positions[2 * i] - aktLat);
+        let dLon = toRad(positions[2 * i + 1] - aktLon);
+        let lat1 = toRad(aktLat);
+        let lat2 = toRad(positions[2 * i]);
 
-        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
             Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c;
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        let d = R * c;
 
         document.getElementById(distances[i]).innerHTML = "&#x1F4CD; " + d.toFixed(2) + " km";
+        distanceList.push(d);
     }
+
+    calculateOrder(distanceList)
 
 }
 
 // Converts numeric degrees to radians
 function toRad(Value) {
     return Value * Math.PI / 180;
+}
+
+function sortElements(sortedList) {
+    let gallery = document.getElementById("gallery")
+
+    for (let i = 1; i < elements.length; i++) {
+        let elem1 = document.getElementById(sortedList[i])
+        let elem2 = document.getElementById(sortedList[elements.length])
+        gallery.insertBefore(elem1, elem2);
+    }
+}
+
+function calculateOrder(distanceList) {
+    let orderedList = elements.copyWithin(elements.length);
+    orderedList.sort((a, b) => {
+        let indexA = elements.indexOf(a);
+        let indexB = elements.indexOf(b);
+        return distanceList[indexA] - distanceList[indexB]
+    })
+    sortElements(orderedList)
+
+
 }
